c07a3906bd43440e18f4b1d1c48e9f38e413b3a1
Najmiddinov Sardorbek <119738684+Sardorbekdev7@users.noreply.github.com>
2024-01-10 14:26:41 +0500

    df

--- src/components/sponsors/Sponsor.jsx
+++ src/components/sponsors/Sponsor.jsx
@@ -30,12 +30,12 @@ const Sponsor = () => {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 5,
-      slidesToSlide: 3 // optional, default to 1.
+      slidesToSlide: 1 // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
-      slidesToSlide: 2 // optional, default to 1.
+      slidesToSlide: 1 // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
